b51e0fe6979a33bba61ed9040973ce346f0f7248
arifmia1129 <arifmia1129@gmail.com>
2023-09-25 17:39:52 +0600

    render role base side items

--- src/components/ui/Sidebar.tsx
+++ src/components/ui/Sidebar.tsx
@@ -2,47 +2,10 @@
 
 import React, { useState } from "react";
 import { Layout, Menu } from "antd";
-import {
-  DesktopOutlined,
-  FileOutlined,
-  PieChartOutlined,
-  TeamOutlined,
-  UserOutlined,
-} from "@ant-design/icons";
-import type { MenuProps } from "antd";
+import { SidebarItems } from "@/constants/sidebarItems";
+import { USER_ROLE } from "@/constants/role";
 
 const { Sider } = Layout;
-
-type MenuItem = Required<MenuProps>["items"][number];
-
-function getItem(
-  label: React.ReactNode,
-  key: React.Key,
-  icon?: React.ReactNode,
-  children?: MenuItem[]
-): MenuItem {
-  return {
-    key,
-    icon,
-    children,
-    label,
-  } as MenuItem;
-}
-
-const items: MenuItem[] = [
-  getItem("Option 1", "1", <PieChartOutlined />),
-  getItem("Option 2", "2", <DesktopOutlined />),
-  getItem("User", "sub1", <UserOutlined />, [
-    getItem("Tom", "3"),
-    getItem("Bill", "4"),
-    getItem("Alex", "5"),
-  ]),
-  getItem("Team", "sub2", <TeamOutlined />, [
-    getItem("Team 1", "6"),
-    getItem("Team 2", "8"),
-  ]),
-  getItem("Files", "9", <FileOutlined />),
-];
 
 const Sidebar = () => {
   const [collapsed, setCollapsed] = useState(false);
@@ -51,13 +14,33 @@ const Sidebar = () => {
       collapsible
       collapsed={collapsed}
       onCollapse={(value) => setCollapsed(value)}
+      width={280}
+      style={{
+        overflow: "auto",
+        height: "100vh",
+        position: "sticky",
+        left: 0,
+        top: 0,
+        bottom: 0,
+      }}
     >
-      <div className="demo-logo-vertical" />
+      <div>
+        <h2
+          style={{
+            color: "#fff",
+            textAlign: "center",
+            marginTop: 10,
+            marginBottom: 5,
+          }}
+        >
+          PH University
+        </h2>
+      </div>
       <Menu
         theme="dark"
         defaultSelectedKeys={["1"]}
         mode="inline"
-        items={items}
+        items={SidebarItems(USER_ROLE.SUPER_ADMIN)}
       />
     </Sider>
   );
