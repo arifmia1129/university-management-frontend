4e7f7833908afeb468a7eadefba60256c03d08ae
arifmia1129 <arifmia1129@gmail.com>
2023-10-04 09:09:57 +0600

    latest

--- src/app/(withlayout)/super_admin/manage-student/create/page.tsx
+++ src/app/(withlayout)/super_admin/manage-student/create/page.tsx
@@ -1,230 +1,60 @@
 "use client";
 
-import Form from "@/components/Forms/Form";
-import FormDatePicker from "@/components/Forms/FormDatePicker";
-import FormInput from "@/components/Forms/FormInput";
-import FormSelectField from "@/components/Forms/FormSelectField";
-import FormTextArea from "@/components/Forms/FormTextArea";
+import GuardianInfo from "@/components/CreateStudent/GuardianInfo";
+import LocalGuardianInfo from "@/components/CreateStudent/LocalGuardianInfo";
+import StudentBasicInfo from "@/components/CreateStudent/StudentBasicInfo";
+import StudentInfo from "@/components/CreateStudent/StudentInfo";
+import StepperForm from "@/components/ui/StepperForm/StepperForm";
 import UMBreadCrumb from "@/components/ui/UMBreadCrumb";
-import Uploader from "@/components/ui/Uploader";
 import { superAdminItems } from "@/constants/breadCrumbItem";
-import {
-  bloodGroupOptions,
-  genderOptions,
-  manageDepartmentOptions,
-} from "@/constants/global";
-import { adminSchema } from "@/schema/admin";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { Button, Col, Row } from "antd";
+import React from "react";
+import { SubmitHandler } from "react-hook-form";
 
-const CreateManagementDepartment = () => {
+const CreateStudentPage = () => {
   const items = [
     ...superAdminItems,
     {
-      label: `manage-admin`,
-      link: `super_admin/admin`,
+      label: `manage-student`,
+      link: `super_admin/manage-student`,
     },
   ];
 
-  const onSubmit = async (data: any) => {
+  const steps = [
+    {
+      title: "Student Information",
+      content: <StudentInfo />,
+    },
+    {
+      title: "Basic Information",
+      content: <StudentBasicInfo />,
+    },
+    {
+      title: "Guardian Information",
+      content: <GuardianInfo />,
+    },
+    {
+      title: "Local Guardian Information",
+      content: <LocalGuardianInfo />,
+    },
+  ];
+
+  const handleStudentInfo = (data: any) => {
     try {
       console.log(data);
     } catch (error) {
-      console.error(error);
+      console.log(error);
     }
   };
 
   return (
     <div>
       <UMBreadCrumb items={items} />
-      <Form submitHandler={onSubmit} resolver={yupResolver(adminSchema)}>
-        <div
-          style={{
-            border: "1px solid #d9d9d9",
-            borderRadius: "10px",
-            padding: "15px",
-            margin: "20px 0",
-          }}
-        >
-          <p style={{ fontSize: "20px", fontWeight: "bold" }}>
-            Admin Information
-          </p>
-          <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-            <Col style={{ margin: "15px 0" }} className="gutter-row" span={8}>
-              <div>
-                <FormInput
-                  name="admin.name.firstName"
-                  type="text"
-                  size="large"
-                  label="First Name"
-                  placeholder="Write first name"
-                />
-              </div>
-            </Col>
-            <Col style={{ margin: "15px 0" }} className="gutter-row" span={8}>
-              <div>
-                <FormInput
-                  name="admin.name.middleName"
-                  type="text"
-                  size="large"
-                  label="Middle Name"
-                  placeholder="Write middle name"
-                />
-              </div>
-            </Col>
-            <Col style={{ margin: "15px 0" }} className="gutter-row" span={8}>
-              <div>
-                <FormInput
-                  name="admin.name.lastName"
-                  type="text"
-                  size="large"
-                  label="Last Name"
-                  placeholder="Write last name"
-                />
-              </div>
-            </Col>
-            <Col style={{ margin: "15px 0" }} className="gutter-row" span={8}>
-              <div>
-                <FormInput
-                  name="password"
-                  type="password"
-                  size="large"
-                  label="Password"
-                  placeholder="Write password"
-                />
-              </div>
-            </Col>
-            <Col style={{ margin: "15px 0" }} className="gutter-row" span={8}>
-              <div>
-                <FormSelectField
-                  name="admin.gender"
-                  size="large"
-                  label="Gender"
-                  items={genderOptions}
-                />
-              </div>
-            </Col>
-            <Col style={{ margin: "15px 0" }} className="gutter-row" span={8}>
-              <div>
-                <FormSelectField
-                  name="admin.manageDepartment"
-                  size="large"
-                  label="Manage Department"
-                  items={manageDepartmentOptions}
-                />
-              </div>
-            </Col>
-            <Col style={{ margin: "15px 0" }} className="gutter-row" span={8}>
-              <div>
-                <Uploader />
-              </div>
-            </Col>
-          </Row>
-        </div>
-        <div
-          style={{
-            border: "1px solid #d9d9d9",
-            borderRadius: "10px",
-            padding: "15px",
-            margin: "20px 0",
-          }}
-        >
-          <p style={{ fontSize: "20px", fontWeight: "bold" }}>
-            Basic Information
-          </p>
-          <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-            <Col style={{ margin: "15px 0" }} className="gutter-row" span={8}>
-              <div>
-                <FormInput
-                  name="admin.email"
-                  type="text"
-                  size="large"
-                  label="Email"
-                  placeholder="Write email"
-                />
-              </div>
-            </Col>
-            <Col style={{ margin: "15px 0" }} className="gutter-row" span={8}>
-              <div>
-                <FormInput
-                  name="admin.contactNo"
-                  type="text"
-                  size="large"
-                  label="Contact Number"
-                  placeholder="Write contact number"
-                />
-              </div>
-            </Col>
-            <Col style={{ margin: "15px 0" }} className="gutter-row" span={8}>
-              <div>
-                <FormInput
-                  name="admin.emergencyContactNo"
-                  type="text"
-                  size="large"
-                  label="Emergency Contact"
-                  placeholder="Write emergency contact number"
-                />
-              </div>
-            </Col>
-            <Col style={{ margin: "15px 0" }} className="gutter-row" span={8}>
-              <div>
-                <FormSelectField
-                  name="admin.bloodGroup"
-                  size="large"
-                  label="Blood Group"
-                  items={bloodGroupOptions}
-                />
-              </div>
-            </Col>
-            <Col style={{ margin: "15px 0" }} className="gutter-row" span={8}>
-              <div>
-                <FormInput
-                  name="admin.designation"
-                  type="text"
-                  size="large"
-                  label="Designation"
-                  placeholder="Write designation"
-                />
-              </div>
-            </Col>
-            <Col style={{ margin: "15px 0" }} className="gutter-row" span={8}>
-              <div>
-                <FormDatePicker
-                  name="admin.dateOfBirth"
-                  size="large"
-                  label="Date of Birth"
-                  placeholder="Write date of birth"
-                />
-              </div>
-            </Col>
-            <Col style={{ margin: "15px 0" }} className="gutter-row" span={12}>
-              <div>
-                <FormTextArea
-                  name="admin.presentAddress"
-                  size="large"
-                  label="Present Address"
-                  placeholder="Write present address"
-                />
-              </div>
-            </Col>
-            <Col style={{ margin: "15px 0" }} className="gutter-row" span={12}>
-              <div>
-                <FormTextArea
-                  name="admin.permanentAddress"
-                  size="large"
-                  label="Permanent Address"
-                  placeholder="Write permanent address"
-                />
-              </div>
-            </Col>
-          </Row>
-        </div>
-        <Button type="primary" htmlType="submit">
-          Create
-        </Button>
-      </Form>
+      <StepperForm
+        submitHander={(value) => handleStudentInfo(value)}
+        steps={steps}
+      />
     </div>
   );
 };
 
-export default CreateManagementDepartment;
+export default CreateStudentPage;
--- src/components/ui/UMTable.tsx
+++ src/components/ui/UMTable.tsx
@@ -1,4 +1,4 @@
-import { Table } from "antd";
+import { Button, Table } from "antd";
 
 const UMTable = () => {
   const columns = [
@@ -11,15 +11,70 @@ const UMTable = () => {
       title: "Age",
       dataIndex: "age",
       key: "age",
+      sorter: (a: any, b: any) => a - b,
     },
     {
       title: "Address",
       dataIndex: "address",
       key: "address",
     },
+    {
+      title: "Action",
+      render: function (data: any) {
+        return (
+          <Button onClick={() => console.log(data)} type="primary" danger>
+            X
+          </Button>
+        );
+      },
+    },
   ];
 
-  return <Table columns={columns} />;
+  const dataSource = [
+    {
+      key: "1",
+      name: "Mike",
+      age: 32,
+      address: "10 Downing Street",
+    },
+    {
+      key: "2",
+      name: "John",
+      age: 42,
+      address: "10 Downing Street",
+    },
+  ];
+
+  const handleChangePaginationOptions = (page: number, size: number): void => {
+    console.log("page:", page, "size:", size);
+  };
+
+  const paginationOptions = {
+    pageSize: 5,
+    total: 10,
+    pageSizeOptions: [5, 10, 20],
+    showSizeChanger: true,
+    onChange: handleChangePaginationOptions,
+  };
+
+  const handleChangeTableOptions = (
+    pagination: any,
+    filter: any,
+    sorter: any
+  ) => {
+    const { order, field } = sorter;
+    console.log(order, field);
+  };
+
+  return (
+    <Table
+      loading={false}
+      columns={columns}
+      dataSource={dataSource}
+      pagination={paginationOptions}
+      onChange={handleChangeTableOptions}
+    />
+  );
 };
 
 export default UMTable;
